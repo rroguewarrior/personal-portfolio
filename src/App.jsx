8d5dcaf7caa78aed39ddccd660fe9fa06b4cfbee
rroguewarrior <126022590+rroguewarrior@users.noreply.github.com>
2023-02-21 21:55:40 +0000

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
+  name: "Pavanteja Konda",
+  title: "Beginner Coder",
   email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  gitHub: "rrogue warrior",
+  instagram: "minecraft game 46",
+  linkedIn: "ultra gamer 46",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "master gamer 46",
+  youTube: "cool coder 46",
 };
 
 const primaryColor = "#4E567E";
